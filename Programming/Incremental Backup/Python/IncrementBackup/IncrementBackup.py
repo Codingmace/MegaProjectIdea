ce75f0517a0787b512fae1a5cc9ac6b6bde1740e
Codingmace <codingmace@gmail.com>
2020-05-15 13:14:47 -0500

    Version 1.4.3

---
+++ Programming/Incremental Backup/Python/IncrementBackup/IncrementBackup.py
@@ -0,0 +1,10 @@
+import zipfile
+
+# Get the folder of all the backups
+# Organize and zip files extracted to folders 1 -10 accordingly
+# Write the changes to be made file using code
+# Adjust the following files with the changes in files IE deleting
+    # Files that are written to can add extension .abby and .jacob
+# Recompress the folders
+
+# Write a decompression function and how that will change
