acfa67abe5f51a595d1d63d1566424b3ed3d5c6a
Codingmace <codingmace@gmail.com>
2020-05-16 17:06:16 -0500

    Version 1.4.5

--- Programming/Incremental Backup/Python/IncrementBackup/IncrementBackup.py
+++ Programming/Incremental Backup/Python/IncrementBackup/IncrementBackup.py
@@ -1,4 +1,26 @@
 import zipfile
+import os
+import fnmatch
+import numpy as np
+
+def f1(fn, dest):
+    with open(fn, 'rb') as f:
+        zf = zipfile.ZipFile(f)
+        zf.extractall(dest)
+
+    total = 0
+    for root, dirs, files in os.walk(dest):
+        for file_ in files:
+            fn = os.path.join(root, file_)
+            total += _count_file(fn)
+    return total
+
+def printFiles(folderpath, pattern):
+    listOfFiles = os.listdir(folderpath)
+    for entry in listOfFiles:
+        if fnmatch.fnmatch(entry, pattern):
+            print (entry)
+    
 
 # Get the folder of all the backups
 # Organize and zip files extracted to folders 1 -10 accordingly
@@ -12,3 +34,12 @@ import zipfile
 # URL TO CHECK
 # https://docs.python.org/3/library/zipfile.html
 # https://www.peterbe.com/plog/fastest-way-to-unzip-a-zip-file-in-python
+
+
+def main():
+    printFiles('TestData', '*.zip')
+    listFiles = os.listdir(".")
+
+#    filename = "~/TestData"
+#    f = open(filename)
+main()
