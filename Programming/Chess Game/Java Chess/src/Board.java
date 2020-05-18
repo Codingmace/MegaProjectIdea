a2b012fd2fa81e717757997f49c6b7b1cd89aa91
Codingmace <codingmace@gmail.com>
2020-05-17 19:54:21 -0500

    Version 1.11.1

--- Programming/Chess Game/Java Chess/src/Board.java
+++ Programming/Chess Game/Java Chess/src/Board.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -15,37 +14,38 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Board extends JPanel implements MouseListener, MouseMotionListener {
-	// Resource location constants for piece images
+    // Resource location constants for piece images
+
     private static final String RESOURCES_WBISHOP_PNG = "wbishop.png";
-	private static final String RESOURCES_BBISHOP_PNG = "bbishop.png";
-	private static final String RESOURCES_WKNIGHT_PNG = "wknight.png";
-	private static final String RESOURCES_BKNIGHT_PNG = "bknight.png";
-	private static final String RESOURCES_WROOK_PNG = "wrook.png";
-	private static final String RESOURCES_BROOK_PNG = "brook.png";
-	private static final String RESOURCES_WKING_PNG = "wking.png";
-	private static final String RESOURCES_BKING_PNG = "bking.png";
-	private static final String RESOURCES_BQUEEN_PNG = "bqueen.png";
-	private static final String RESOURCES_WQUEEN_PNG = "wqueen.png";
-	private static final String RESOURCES_WPAWN_PNG = "wpawn.png";
-	private static final String RESOURCES_BPAWN_PNG = "bpawn.png";
-	
-	// Logical and graphical representations of board
-	private final Square[][] board;
+    private static final String RESOURCES_BBISHOP_PNG = "bbishop.png";
+    private static final String RESOURCES_WKNIGHT_PNG = "wknight.png";
+    private static final String RESOURCES_BKNIGHT_PNG = "bknight.png";
+    private static final String RESOURCES_WROOK_PNG = "wrook.png";
+    private static final String RESOURCES_BROOK_PNG = "brook.png";
+    private static final String RESOURCES_WKING_PNG = "wking.png";
+    private static final String RESOURCES_BKING_PNG = "bking.png";
+    private static final String RESOURCES_BQUEEN_PNG = "bqueen.png";
+    private static final String RESOURCES_WQUEEN_PNG = "wqueen.png";
+    private static final String RESOURCES_WPAWN_PNG = "wpawn.png";
+    private static final String RESOURCES_BPAWN_PNG = "bpawn.png";
+
+    // Logical and graphical representations of board
+    private final Square[][] board;
     private final GameWindow g;
-    
+
     // List of pieces and whether they are movable
     public final LinkedList<Piece> Bpieces;
     public final LinkedList<Piece> Wpieces;
     public List<Square> movable;
-    
+
     private boolean whiteTurn;
 
     private Piece currPiece;
     private int currX;
     private int currY;
-    
+
     private CheckmateDetector cmd;
-    
+
     public Board(GameWindow g) {
         this.g = g;
         board = new Square[8][8];
@@ -83,15 +83,15 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     }
 
     private void initializePieces() {
-    	
+
         for (int x = 0; x < 8; x++) {
             board[1][x].put(new Pawn(0, board[1][x], RESOURCES_BPAWN_PNG));
             board[6][x].put(new Pawn(1, board[6][x], RESOURCES_WPAWN_PNG));
         }
-        
+
         board[7][3].put(new Queen(1, board[7][3], RESOURCES_WQUEEN_PNG));
         board[0][3].put(new Queen(0, board[0][3], RESOURCES_BQUEEN_PNG));
-        
+
         King bk = new King(0, board[0][4], RESOURCES_BKING_PNG);
         King wk = new King(1, board[7][4], RESOURCES_WKING_PNG);
         board[0][4].put(bk);
@@ -111,15 +111,14 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         board[0][5].put(new Bishop(0, board[0][5], RESOURCES_BBISHOP_PNG));
         board[7][2].put(new Bishop(1, board[7][2], RESOURCES_WBISHOP_PNG));
         board[7][5].put(new Bishop(1, board[7][5], RESOURCES_WBISHOP_PNG));
-        
-        
-        for(int y = 0; y < 2; y++) {
+
+        for (int y = 0; y < 2; y++) {
             for (int x = 0; x < 8; x++) {
                 Bpieces.add(board[y][x].getOccupyingPiece());
-                Wpieces.add(board[7-y][x].getOccupyingPiece());
+                Wpieces.add(board[7 - y][x].getOccupyingPiece());
             }
         }
-        
+
         cmd = new CheckmateDetector(this, Wpieces, Bpieces, wk, bk);
     }
 
@@ -168,10 +167,12 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 
         if (sq.isOccupied()) {
             currPiece = sq.getOccupyingPiece();
-            if (currPiece.getColor() == 0 && whiteTurn)
+            if (currPiece.getColor() == 0 && whiteTurn) {
                 return;
-            if (currPiece.getColor() == 1 && !whiteTurn)
+            }
+            if (currPiece.getColor() == 1 && !whiteTurn) {
                 return;
+            }
             sq.setDisplay(false);
         }
         repaint();
@@ -182,10 +183,12 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         Square sq = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
 
         if (currPiece != null) {
-            if (currPiece.getColor() == 0 && whiteTurn)
+            if (currPiece.getColor() == 0 && whiteTurn) {
                 return;
-            if (currPiece.getColor() == 1 && !whiteTurn)
+            }
+            if (currPiece.getColor() == 1 && !whiteTurn) {
                 return;
+            }
 
             List<Square> legalMoves = currPiece.getLegalMoves(this);
             movable = cmd.getAllowableSquares(whiteTurn);
--- Programming/Chess Game/Java Chess/src/StartMenu.java
+++ Programming/Chess Game/Java Chess/src/StartMenu.java
@@ -4,6 +4,8 @@ import java.awt.Component;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.Box;
@@ -17,57 +19,59 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class StartMenu implements Runnable {
+
+    @Override
     public void run() {
         final JFrame startWindow = new JFrame("Chess");
         // Set window properties
-        startWindow.setLocation(300,100);
+        startWindow.setLocation(300, 100);
         startWindow.setResizable(false);
         startWindow.setSize(260, 240);
-        
+
         Box components = Box.createVerticalBox();
         startWindow.add(components);
-        
+
         // Game title
         final JPanel titlePanel = new JPanel();
         components.add(titlePanel);
         final JLabel titleLabel = new JLabel("Chess");
         titlePanel.add(titleLabel);
-        
+
         // Black player selections
         final JPanel blackPanel = new JPanel();
         components.add(blackPanel, BorderLayout.EAST);
         final JLabel blackPiece = new JLabel();
         try {
-            Image blackImg = ImageIO.read(getClass().getResource("bp.png"));
+//            System.out.println(ImageIO.read(new File("resoures\\bp.png")));
+//            System.out.println(ImageIO.read(new File("resources/bp.png")));
+            Image blackImg = ImageIO.read(new File("resources/bp.png"));
+//            Image blackImg = ImageIO.read(getClass().getResource("resources/bp.png"));
             blackPiece.setIcon(new ImageIcon(blackImg));
             blackPanel.add(blackPiece);
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Required game file bp.png missing");
         }
-        
-        
-        
+
         final JTextField blackInput = new JTextField("Black", 10);
         blackPanel.add(blackInput);
-        
+
         // White player selections
         final JPanel whitePanel = new JPanel();
         components.add(whitePanel);
         final JLabel whitePiece = new JLabel();
-        
+
         try {
-            Image whiteImg = ImageIO.read(getClass().getResource("wp.png"));
+            Image whiteImg = ImageIO.read(new File("resources/wp.png"));
             whitePiece.setIcon(new ImageIcon(whiteImg));
             whitePanel.add(whitePiece);
             startWindow.setIconImage(whiteImg);
-        }  catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Required game file wp.png missing");
         }
-        
-        
+
         final JTextField whiteInput = new JTextField("White", 10);
         whitePanel.add(whiteInput);
-        
+
         // Timer settings
         final String[] minSecInts = new String[60];
         for (int i = 0; i < 60; i++) {
@@ -77,54 +81,52 @@ public class StartMenu implements Runnable {
                 minSecInts[i] = Integer.toString(i);
             }
         }
-        
+
         final JComboBox<String> seconds = new JComboBox<>(minSecInts);
         final JComboBox<String> minutes = new JComboBox<>(minSecInts);
-        final JComboBox<String> hours = 
-                new JComboBox<>(new String[] {"0","1","2","3"});
-        
+        final JComboBox<String> hours
+                = new JComboBox<>(new String[]{"0", "1", "2", "3"});
+
         Box timerSettings = Box.createHorizontalBox();
-        
+
         hours.setMaximumSize(hours.getPreferredSize());
         minutes.setMaximumSize(minutes.getPreferredSize());
         seconds.setMaximumSize(minutes.getPreferredSize());
-        
+
         timerSettings.add(hours);
         timerSettings.add(Box.createHorizontalStrut(10));
         timerSettings.add(seconds);
         timerSettings.add(Box.createHorizontalStrut(10));
         timerSettings.add(minutes);
-        
+
         timerSettings.add(Box.createVerticalGlue());
-        
+
         components.add(timerSettings);
-        
+
         // Buttons
         Box buttons = Box.createHorizontalBox();
         final JButton quit = new JButton("Quit");
-        
-        quit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-              startWindow.dispose();
-            }
-          });
-        
+
+        quit.addActionListener((ActionEvent e) -> {
+            startWindow.dispose();
+        });
+
         final JButton instr = new JButton("Instructions");
-        
+
         instr.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(startWindow,
-                        "To begin a new game, input player names\n" +
-                        "next to the pieces. Set the clocks and\n" +
-                        "click \"Start\". Setting the timer to all\n" +
-                        "zeroes begins a new untimed game.",
+                        "To begin a new game, input player names\n"
+                        + "next to the pieces. Set the clocks and\n"
+                        + "click \"Start\". Setting the timer to all\n"
+                        + "zeroes begins a new untimed game.",
                         "How to play",
                         JOptionPane.PLAIN_MESSAGE);
             }
-          });
-        
+        });
+
         final JButton start = new JButton("Start");
-        
+
         start.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String bn = blackInput.getText();
@@ -132,19 +134,19 @@ public class StartMenu implements Runnable {
                 int hh = Integer.parseInt((String) hours.getSelectedItem());
                 int mm = Integer.parseInt((String) minutes.getSelectedItem());
                 int ss = Integer.parseInt((String) seconds.getSelectedItem());
-                
+
                 new GameWindow(bn, wn, hh, mm, ss);
                 startWindow.dispose();
             }
-          });
-        
+        });
+
         buttons.add(start);
         buttons.add(Box.createHorizontalStrut(10));
         buttons.add(instr);
         buttons.add(Box.createHorizontalStrut(10));
         buttons.add(quit);
         components.add(buttons);
-        
+
         Component space = Box.createGlue();
         components.add(space);
 
